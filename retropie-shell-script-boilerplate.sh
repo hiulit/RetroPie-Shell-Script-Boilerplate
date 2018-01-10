a2b4c5c01571d4cc5c759788f2c19ef25c634cd5
Xavier Gómez Gosálbez <hiulit@gmail.com>
2018-01-10 10:47:30 +0100

    Update retropie-shell-script-boilerplate.sh

--- retropie-shell-script-boilerplate.sh
+++ retropie-shell-script-boilerplate.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-# Retropie Shell Script Boilerplate.
-# A template for building shell scripts for RetroPie.
+# [SCRIPT_TITLE] (e.g. Retropie Shell Script Boilerplate.)
+# [SCRIPT_DESCRIPTION] (e.g. A template for building shell scripts for RetroPie.)
+#
+# Author: [AUTHOR] (e.g. hiulit)
+# Repository: [REPOSITORY_URL] (e.g. https://github.com/hiulit/RetroPie-Shell-Script-Boilerplate)
+# Issues: [REPOSITORY_ISSUES_URL] (e.g. https://github.com/hiulit/RetroPie-Shell-Script-Boilerplate/issues)
+# License: [LICENSE] [LICENSE_URL] (e.g. MIT https://github.com/hiulit/RetroPie-Shell-Script-Boilerplate/blob/master/LICENSE)
 #
 # Requirements:
 # - RetroPie x.x.x (e.g. RetroPie 4.x.x)
@@ -10,9 +15,13 @@
 home="$(find /home -type d -name RetroPie -print -quit 2>/dev/null)"
 home="${home%/RetroPie}"
 
+readonly SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 readonly SCRIPT_TITLE=""
 readonly SCRIPT_DESCRIPTION=""
-readonly SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+# If you want to use a config file, uncomment the variable below and enter a name.
+# readonly SCRIPT_CFG="$SCRIPT_DIR/[CONFIG_FILE_NAME]"
+
+# Add more variables here.
 
 function is_retropie() {
     [[ -d "$home/RetroPie" && -d "$home/.emulationstation" && -d "/opt/retropie" ]]
@@ -38,9 +47,27 @@ function check_argument() {
     fi
 }
 
+# If you are using a config file, uncomment set_config() and get_config().
+# USAGE:
+# set_config "[KEY]" "[VALUE]" - Sets the VALUE to the KEY
+# get_config "[KEY]" - Returns the KEY's VALUE
+
+# function set_config() {
+#     sed -i "s|^\($1\s*=\s*\).*|\1\"$2\"|" "$SCRIPT_CFG"
+#     echo "\"$1\" set to \"$2\"."
+# }
+
+# function get_config() {
+#     local config
+#     config="$(grep -Po "(?<=^$1 = ).*" "$SCRIPT_CFG")"
+#     config="${config%\"}"
+#     config="${config#\"}"
+#     echo "$config"
+# }
+
 function usage() {
     echo
-    echo "USAGE: $0 [options]"
+    echo "USAGE: $0 [OPTIONS]" # Add 'sudo' before '$0' if the scripts needs to be run under sudo (e.g. USAGE: sudo $0 [OPTIONS]).
     echo
     echo "Use '--help' to see all the options."
     echo
@@ -54,9 +81,7 @@ function get_options() {
     if [[ -z "$1" ]]; then
         usage
         exit 0
-    fi
-
-    while [[ -n "$1" ]]; do
+    else
         case "$1" in
 #H -h, --help                                   Print the help message and exit.
             -h|--help)
@@ -64,7 +89,7 @@ function get_options() {
                 echo "$SCRIPT_TITLE"
                 echo "$SCRIPT_DESCRIPTION"
                 echo
-                echo "USAGE: $0 [options]"
+                echo "USAGE: $0 [OPTIONS]" # Add 'sudo' before '$0' if the scripts needs to be run under sudo (e.g. USAGE: sudo $0 [OPTIONS]).
                 echo
                 echo "OPTIONS:"
                 echo
@@ -75,28 +100,35 @@ function get_options() {
 
 #H -o, --option (e.g '-v, --version')       Description of the option (e.g. Show version of the script).       
             -o|--option)
-                # If the option has argument, uncomment the code below. If not, just delete it.
+                # If the option has arguments, uncomment the code below.
                 # check_argument "$1" "$2" || exit 1
                 # shift
                 
                 # Add the functions for this options here.
                 ;;        
             *)
-                echo "ERROR: invalid option \"$1\"" >&2
+                echo "ERROR: invalid option '$1'" >&2
                 exit 2
                 ;;
         esac
-        shift
-    done
+    fi
 }
 
 function main() {
-    check_dependencies
-
     if ! is_retropie; then
         echo "ERROR: RetroPie is not installed. Aborting ..." >&2
         exit 1
     fi
+    
+    check_dependencies
+    
+    # If you need to check if sudo is used, uncomment the code below.
+    # Remember to add 'sudo' in 'usage' and 'help'.
+    # if [[ "$(id -u)" -ne 0 ]]; then
+    #     echo "ERROR: Script must be run under sudo." >&2
+    #     usage
+    #     exit 1
+    # fi
 
     get_options "$@"
 }
